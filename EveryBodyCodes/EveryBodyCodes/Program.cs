9b203a60c48ec86e76693e1458a7bb4852380abf
Souji Kenzaki <chenuki@yahoo.co.jp>
2020-11-07 17:27:21 +0900

    Rebuilding basis

--- EveryBodyCodes/EveryBodyCodes/Program.cs
+++ EveryBodyCodes/EveryBodyCodes/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace EveryBodyCodesApi
+namespace EveryBodyCodes
 {
     public class Program
     {
